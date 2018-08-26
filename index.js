a25b52c0767df4f5b9e8c3ac79a48fed83cdc53e
github username <github email address>
2018-08-26 16:02:13 -0400

    Look at code below.  If youre destructive, you call the original obj

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+const driver = {};
+
+function updateDriverWithKeyAndValue (driver, key, value) {
+  const newDriver = {...driver};
+  newDriver[key] = value;
+  return newDriver
+}
+
+function destructivelyUpdateDriverWithKeyAndValue (driver, key, value) {
+  driver[key] = value;
+  return driver
+}
+
+function deleteFromDriverByKey(driver, key) {
+  const newdriver = Object.assign({}, driver);
+  delete newdriver[key];
+  return newdriver
+
+}
+
+function destructivelyDeleteFromDriverByKey (driver, key) {
+  delete driver[key];
+  return driver
+}
